c784d8dd1c94b1b2c63b6c40e8937d1373a58295
rtritto <ariele.1994@gmail.com>
2026-07-17 13:31:01 +0200

    refactor: +Layout export and rename layouts to styles

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
   plugins: { vue },
   settings: {
     tailwindcss: {
-      cssConfigPath: './src/layouts/tailwind.css'
+      cssConfigPath: './src/styles/tailwind.css'
     }
   },
   rules: {
--- src/pages/+Layout.vue
+++ src/pages/+Layout.vue
@@ -1,30 +1,7 @@
 <template>
-  <div style="display: flex; max-width: 900px; margin: auto;">
-    <div id="sidebar" style="
-        padding: 20px;
-        flex-shrink: 0;
-        display: flex;
-        flex-direction: column;
-        line-height: 1.8em;
-        border-right: 2px solid #eee;
-      ">
-      <Link href="/">Welcome</Link>
-    </div>
-
-    <div id="page-container">
-      <div id="page-content" style="
-          padding: 20px;
-          padding-bottom: 50px;
-          min-height: 100vh;
-        ">
-        <slot />
-      </div>
-    </div>
-  </div>
+  <Layout />
 </template>
 
 <script setup lang="ts">
-import '../layouts/styles.css'
-import '../layouts/tailwind.css'
-import Link from '@/components/Link.vue'
+import Layout from '@/components/Layout.vue'
 </script>
