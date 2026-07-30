b7c7e7aae24bf6ef4611e99079c90f04e8091f3f
rtritto <ariele.1994@gmail.com>
2026-07-30 23:54:54 +0200

    chore(deps): remove standaloner and rolldown

--- vite.config.ts
+++ vite.config.ts
@@ -12,13 +12,7 @@ export default {
     vikeLite({
       serverEntry: 'server/index.ts'
     }),
-    vikeLiteVue(),
-    ...process.env.NODE_ENV === 'production' ? [
-      (await import('standaloner/vite')).default({
-        bundle: true,
-        minify: true
-      })
-    ] : []
+    vikeLiteVue()
   ],
   server: {
     port: 3000
